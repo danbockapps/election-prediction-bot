48296c7576bcc72ba2828925aac5323056072812
Dan Bock <danbock@gmail.com>
2020-06-13 17:03:29 -0400

    Log data every 30 minutes

--- functions/src/economist.ts
+++ functions/src/economist.ts
@@ -2,11 +2,19 @@ import axios from 'axios'
 import Papa from 'papaparse'
 require('dotenv').config()
 
+interface EcProbDataPoint {
+  date: string
+  party: 'democratic' | 'republican'
+  win_prob: number
+}
 
 export const getData = () =>
   axios
     .get<string>(
       'https://cdn.economistdatateam.com/us-2020-forecast/data/president/electoral_college_probability_over_time.csv',
     )
-    .then(response => Papa.parse(response.data, { header: true }).data)
-
+    .then(response =>
+      Papa.parse<EcProbDataPoint>(response.data, {
+        header: true,
+      }).data.filter(e => e.date),
+    )
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import { getData } from './economist'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,14 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const tweetEconomistEcProb = functions.pubsub
+  .schedule('19,49 * * * *')
+  .timeZone('America/New_York')
+  .onRun(async () => {
+    const data = await getData()
+    console.log(JSON.stringify(data[data.length - 4]))
+    console.log(JSON.stringify(data[data.length - 3]))
+    console.log(JSON.stringify(data[data.length - 2]))
+    console.log(JSON.stringify(data[data.length - 1]))
+  })
